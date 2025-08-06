64b2d81dfe3b2eb3d89896c3e0444776bf8c9f2d
João Vitor <63624487+eumoitinho@users.noreply.github.com>
2025-08-06 03:35:07 -0300

    fix: update TinaCMS client configuration to use environment variables and branch reference

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,9 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ 
-  url: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "http://localhost:4001",
-  token: process.env.TINA_TOKEN , 
-  queries
-});
+export const client = createClient({ cacheDir: 'D:/CATALISTI/M2Z/PROPRIETARIO/m2z-control/site_diray/tina/__generated__/.cache/1754462062577', url: 'https://content.tinajs.io/1.6/content/ebb20b99-d45a-4ba2-8f1e-8461e821fde9/github/dependabot/npm_and_yarn/multi-544f560e85', token: '03e16881262879a322f1d5f6a714dc6f32201dc1', queries,  });
 export default client;
   
