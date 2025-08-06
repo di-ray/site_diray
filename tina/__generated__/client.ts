687968f490baadcbdea720f05e60b16a6d25e658
João Vitor <63624487+eumoitinho@users.noreply.github.com>
2025-08-06 02:51:15 -0300

    Refactor code structure for improved readability and maintainability

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ 
+  url: 'https://content.tinajs.io/1.6/content/ebb20b99-d45a-4ba2-8f1e-8461e821fde9/github/dependabot/npm_and_yarn/multi-544f560e85', 
+  token: '03e16881262879a322f1d5f6a714dc6f32201dc1', 
+  queries
+});
+export default client;
+  
