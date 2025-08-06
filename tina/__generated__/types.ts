aca4663920dd5d0a0a285720f2f48367d99f18d2
João Vitor <63624487+eumoitinho@users.noreply.github.com>
2025-08-06 15:39:22 -0300

    fix: refactor TinaCMS client to use self-hosted configuration and update branch reference

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -2647,7 +2647,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "https://content.tinajs.io/1.6/content/ebb20b99-d45a-4ba2-8f1e-8461e821fde9/github/dependabot/npm_and_yarn/multi-544f560e85",
+        url: "http://localhost:3000/api/tina/graphql",
         queries,
       })
     )
--- tina/config.tsx
+++ tina/config.tsx
@@ -6,12 +6,15 @@ import { FAQCollection } from "./collections/faq";
 import { SolutionCollection } from "./collections/solution";
 import { CalculatorCollection } from "./collections/calculator";
 
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === 'true';
 const branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || "dependabot/npm_and_yarn/multi-544f560e85";
 
 export default defineConfig({
   // Self-hosted configuration
-  contentApiUrlOverride: "/api/tina/graphql",
+  contentApiUrlOverride: isLocal ? `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/api/tina/graphql` : "/api/tina/graphql",
   branch,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
   
   build: {
     publicFolder: "public",
